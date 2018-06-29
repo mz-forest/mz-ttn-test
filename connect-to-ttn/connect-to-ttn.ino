c39c1e6680df762b570ebfd8ec35423b002cc4ba
Stefan Huber <huebe@huebe.com>
2018-06-29 11:27:48 +0200

    let's get it right this time

--- connect-to-ttn/connect-to-ttn.ino
+++ connect-to-ttn/connect-to-ttn.ino
@@ -1,8 +1,8 @@
 #include <TheThingsNetwork.h>
 
 // Set your AppEUI and AppKey
-const char *appEui = "GETITFROMTTN";
-const char *appKey = "GETITFROMTTN";
+const char *appEui = "GET IT FROM TTN";
+const char *appKey = "GET IT FROM TTN";
 
 #define loraSerial Serial1
 #define debugSerial Serial
@@ -18,8 +18,15 @@ void setup()
   debugSerial.begin(9600);
 
   // Wait a maximum of 10s for Serial Monitor
-  while (!debugSerial && millis() < 10000)
-    ;
+  while (!debugSerial && millis() < 10000);
+
+  //============= do your sensor setup here ================
+
+
+  //============= End of do your sensor setup here =========
+
+  // Set callback for incoming messages
+  ttn.onMessage(message);
 
   debugSerial.println("-- STATUS");
   ttn.showStatus();
@@ -32,12 +39,38 @@ void loop()
 {
   debugSerial.println("-- LOOP");
 
+  //============= do your sensor work here ================
+
+
+  //============= End of do your sensor work here =========
+
   // Prepare payload of 1 byte to indicate LED status
   byte payload[1];
   payload[0] = (digitalRead(LED_BUILTIN) == HIGH) ? 1 : 0;
-
   // Send it off
   ttn.sendBytes(payload, sizeof(payload));
 
   delay(10000);
+}
+
+void message(const byte *payload, size_t length, port_t port)
+{
+  debugSerial.println("-- MESSAGE");
+
+  // Only handle messages of a single byte
+  if (length != 1)
+  {
+    return;
+  }
+
+  if (payload[0] == 0)
+  {
+    debugSerial.println("LED: off");
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  else if (payload[0] == 1)
+  {
+    debugSerial.println("LED: on");
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
 }
