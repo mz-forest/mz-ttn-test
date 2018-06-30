c8a0c009de587df1d48d67a14b8dd1bf4501f8f6
huebe <huebe@huebe.com>
2018-06-30 14:37:38 +0200

    name dropping to conf-badge maker

--- badge-hack/mz-badge-sensor/mz-badge-sensor.ino
+++ badge-hack/mz-badge-sensor/mz-badge-sensor.ino
@@ -9,7 +9,7 @@
  * 
  * The front buttons are used to count Horses, Hikers, Bikers and maybe Zombies.
  * 
- * botched, together by Stefan Huber, inspired by the samples found in the badge repo
+ * botched, together by Stefan Huber, inspired by the samples found in the badge repo of Michel Racic
  * as from a hackaton, the code wild and unstructured ;)
  */
 
